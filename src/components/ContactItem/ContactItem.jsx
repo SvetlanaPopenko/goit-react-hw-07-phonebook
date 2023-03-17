d221e06250167716d7ff9c15bc420b5548243af7
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-18 01:41:55 +0200

    Update ContactItem.jsx

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,14 +3,14 @@ import { deleteContact } from 'redux/operations';
 import { ContactNumber, ContactListButton } from './ContactItem.styled';
 import PropTypes from 'prop-types';
 
-const ContactItem = ({ name, number, id }) => {
+const ContactItem = ({ contact: { name, phone, id } }) => {
   const dispatch = useDispatch();
     const handleDeleteContact = () => dispatch(deleteContact(id));
     
   return (
     <>
       <p>
-        {name}: <ContactNumber>{number}</ContactNumber>
+        {name}: <ContactNumber>{phone}</ContactNumber>
       </p>
       <ContactListButton onClick={handleDeleteContact}>
         Delete
@@ -21,8 +21,8 @@ const ContactItem = ({ name, number, id }) => {
 
 ContactItem.propTypes = {
   id: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  phone: PropTypes.string,
 };
 
 export default ContactItem;
